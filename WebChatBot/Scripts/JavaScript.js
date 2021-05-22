4472134d27d48838f251b85a45cdd1a9569b373e
Paulo André <andre.oliveira.belo@gmail.com>
2021-05-21 23:07:01 -0300

    ajustes e criação de novas interações atraves das requisições

--- WebChatBot/Scripts/JavaScript.js
+++ WebChatBot/Scripts/JavaScript.js
@@ -1,8 +1,148 @@
 ﻿
+function takebe() {
+    //debugger;
+    var mensagem = $("#mensagem").val();
+    var stringUrl = "/RespostaChats/Chat";
+
+    //debugger;
+    $.ajax({
+        type: "POST",
+        url: stringUrl,
+        async: false,
+        data: { mensagem: 'take.be' },
+        success: function (data) {
+            $("#displaymensagem").append("<br> <div class='badge badge-secondary'><b>  Eu :</b> " + mensagem + "</div>");
+            $("#displaymensagem").append("<br> <div class='card-login' style='text-align: justify;background-color:#b2dfdb!important;height:12%;box-shadow:none;margin:10px;'><div style='margin:10px;'><b> Lora :</b> " + data.resposta + "</div></div>");
+            $("#mensagem").val("");
+            // debugger;
+        }
+    });
+    //  debugger;
+    $('#takebe').click(function () {
+        $('#displaymensagem').animate({ scrollTop: $('#displaymensagem')[0].scrollHeight }, 500);
+    })
+}
+function taketeam() {
+    //debugger;
+    var mensagem = $("#mensagem").val();
+    var stringUrl = "/RespostaChats/Chat";
+
+    //debugger;
+    $.ajax({
+        type: "POST",
+        url: stringUrl,
+        async: false,
+        data: { mensagem: 'take.team' },
+        success: function (data) {
+            $("#displaymensagem").append("<br> <div class='badge badge-secondary'><b>  Eu :</b> " + mensagem + "</div>");
+            $("#displaymensagem").append("<br> <div class='card-login' style='text-align: justify;background-color:#b2dfdb!important;height:12%;box-shadow:none;margin:10px;'><div style='margin:10px;'><b> Lora :</b> " + data.resposta + "</div></div>");
+            $("#mensagem").val("");
+            // debugger;
+        }
+    });
+    //  debugger;
+    $('#taketeam').click(function () {
+        $('#displaymensagem').animate({ scrollTop: $('#displaymensagem')[0].scrollHeight }, 500);
+    })
+}
+
+function takeexc() {
+    //debugger;
+    var mensagem = $("#mensagem").val();
+    var stringUrl = "/RespostaChats/Chat";
+
+    //debugger;
+    $.ajax({
+        type: "POST",
+        url: stringUrl,
+        async: false,
+        data: { mensagem: 'Take.Execellence' },
+        success: function (data) {
+            $("#displaymensagem").append("<br> <div class='badge badge-secondary'><b>  Eu :</b> " + mensagem + "</div>");
+            $("#displaymensagem").append("<br> <div class='card-login' style='text-align: justify;background-color:#b2dfdb!important;height:12%;box-shadow:none;margin:10px;'><div style='margin:10px;'><b> Lora :</b> " + data.resposta + "</div></div>");
+            $("#mensagem").val("");
+            // debugger;
+        }
+    });
+    //  debugger;
+    $('#takeexc').click(function () {
+        $('#displaymensagem').animate({ scrollTop: $('#displaymensagem')[0].scrollHeight }, 500);
+    })
+}
+
+function takechange() {
+    //debugger;
+    var mensagem = $("#mensagem").val();
+    var stringUrl = "/RespostaChats/Chat";
+
+    //debugger;
+    $.ajax({
+        type: "POST",
+        url: stringUrl,
+        async: false,
+        data: { mensagem: 'Take.Charge' },
+        success: function (data) {
+            $("#displaymensagem").append("<br> <div class='badge badge-secondary'><b>  Eu :</b> " + mensagem + "</div>");
+            $("#displaymensagem").append("<br> <div class='card-login' style='text-align: justify;background-color:#b2dfdb!important;height:12%;box-shadow:none;margin:10px;'><div style='margin:10px;'><b> Lora :</b> " + data.resposta + "</div></div>");
+            $("#mensagem").val("");
+            // debugger;
+        }
+    });
+    //  debugger;
+    $('#takechange').click(function () {
+        $('#displaymensagem').animate({ scrollTop: $('#displaymensagem')[0].scrollHeight }, 500);
+    })
+}
+
+function takesimple() {
+    //debugger;
+    var mensagem = $("#mensagem").val();
+    var stringUrl = "/RespostaChats/Chat";
+
+    //debugger;
+    $.ajax({
+        type: "POST",
+        url: stringUrl,
+        async: false,
+        data: { mensagem: 'Take.Simple' },
+        success: function (data) {
+            $("#displaymensagem").append("<br> <div class='badge badge-secondary'><b>  Eu :</b> " + mensagem + "</div>");
+            $("#displaymensagem").append("<br> <div class='card-login' style='text-align: justify;background-color:#b2dfdb!important;height:12%;box-shadow:none;margin:10px;'><div style='margin:10px;'><b> Lora :</b> " + data.resposta + "</div></div>");
+            $("#mensagem").val("");
+            // debugger;
+        }
+    });
+    //  debugger;
+    $('#takesimple').click(function () {
+        $('#displaymensagem').animate({ scrollTop: $('#displaymensagem')[0].scrollHeight }, 500);
+    })
+}
+
+function takehigh() {
+    //debugger;
+    var mensagem = $("#mensagem").val();
+    var stringUrl = "/RespostaChats/Chat";
+
+    //debugger;
+    $.ajax({
+        type: "POST",
+        url: stringUrl,
+        async: false,
+        data: { mensagem: 'Take.Higher' },
+        success: function (data) {
+            $("#displaymensagem").append("<br> <div class='badge badge-secondary'><b>  Eu :</b> " + mensagem + "</div>");
+            $("#displaymensagem").append("<br> <div class='card-login' style='text-align: justify;background-color:#b2dfdb!important;height:12%;box-shadow:none;margin:10px;'><div style='margin:10px;'><b> Lora :</b> " + data.resposta + "</div></div>");
+            $("#mensagem").val("");
+            // debugger;
+        }
+    });
+    //  debugger;
+    $('#takehigh').click(function () {
+        $('#displaymensagem').animate({ scrollTop: $('#displaymensagem')[0].scrollHeight }, 500);
+    })
+}
 $(function () {
-    //Inicio da conversa no carregamento da página
-
-
+    //Inicio da conversa no carregamento da página    
     function inicioConversa() {
         //debugger;
         var mensagem = $("#mensagem").val();
@@ -16,7 +156,14 @@ $(function () {
             data: { mensagem: "inicioChat" },
             success: function (data) {
                 //$("#displaymensagem").append(" >> EU : " + mensagem + "\n");
-                $("#displaymensagem").append("<div class='col-md-9' style=''><div class='card-login' style='text-align: justify;background-color:#b2dfdb!important;height:18%;box-shadow:none;'><div style='margin:10px;'>" + data.resposta + "</div></div></div><br><br><br><br>");
+                $("#displaymensagem").append("<div class='col-md-12' style=''><div class='card-login' style='text-align: justify;background-color:#b2dfdb!important;height:15%;box-shadow:none;'><div style='margin:10px;'>" + data.resposta + "</div></div></div><br><br><br><br>");
+                $("#displaymensagem").append("<div class='container'>");
+                $("#displaymensagem").append("<div class='col-md-4' ><a onclick='takebe()' id='takebe'><img style='width:60%;' src='../Content/images/img_00.png'></a></div>");
+                $("#displaymensagem").append("<div class='col-md-4' ><a onclick='taketeam()' id='taketeam'><img style='width:60%;' src='../Content/images/img_01.png'></a></div>");
+                $("#displaymensagem").append("<div class='col-md-4' ><a onclick='takeexc()' id='takeexc'><img style='width:60%;' src='../Content/images/img_02.png'></a></div>");
+                $("#displaymensagem").append("<div class='col-md-4' ><a onclick='takechange()' id='takechange'><img style='width:60%;' src='../Content/images/img_03.png'></a></div>");
+                $("#displaymensagem").append("<div class='col-md-4' ><a onclick='takesimple()' id='takesimple'><img style='width:60%;' src='../Content/images/img_04.png'></a></div>");
+                $("#displaymensagem").append("<div class='col-md-4' ><a onclick='takehigh()' id='takehigh'><img style='width:60%;' src='../Content/images/img_05.png'></a></div></div>");
                 $("#mensagem").val("");
                 // debugger;
             }
@@ -44,6 +191,7 @@ $(function () {
     });
 
 
+
     $("#Enviar").click(
         function () {
             //debugger;
@@ -58,12 +206,15 @@ $(function () {
                 data: { mensagem: mensagem },
                 success: function (data) {
                     $("#displaymensagem").append("<br> <div class='badge badge-secondary'><b>  Eu :</b> " + mensagem + "</div>");
-                    $("#displaymensagem").append("<br> <b>  Lora :</b> " + data.resposta + "");
+                    $("#displaymensagem").append("<br> <div class='card-login' style='text-align: justify;background-color:#b2dfdb!important;height:12%;box-shadow:none;margin:10px;'><div style='margin:10px;'><b> Lora :</b> " + data.resposta + "</div></div>");
                     $("#mensagem").val("");
                     // debugger;
                 }
             });
             //  debugger;
+            $('#Enviar').click(function () {
+                $('#displaymensagem').animate({ scrollTop: $('#displaymensagem')[0].scrollHeight }, 500);
+            })
         }
     );
 
